201c3f9ee4760a79e430871b3b1e2ba59c500682
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-12-01 14:27:41 -0300

    :sparkles: feat: Padrão de projeto Observer para atualizar a TableView

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,6 +1,7 @@
 package gui;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -15,11 +16,14 @@ import model.entities.Department;
 import model.service.DepartmentService;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class DepartmentFormController implements Initializable {
     private Department entity;
     private DepartmentService service;
+    private List<DataChangeListener> dataChangeListeners=new ArrayList<>();
     @FXML
     private TextField txtId;
     @FXML
@@ -37,6 +41,9 @@ public class DepartmentFormController implements Initializable {
     public void setDepartmentService(DepartmentService service){
         this.service=service;
     }
+    public void subscribeDataChangeListener(DataChangeListener listener){
+        dataChangeListeners.add(listener);
+    }
 
     @FXML
     public void onBtSaveAction(ActionEvent event){
@@ -44,9 +51,16 @@ public class DepartmentFormController implements Initializable {
         try{
             entity = getFormData();
             service.saveOrUpdate(entity);
+            notifyDataChangeListeners();
             Utils.currentStage(event).close();
         }catch (DbException e){
             Alerts.showAlerts("Error saving object",null,e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    private void notifyDataChangeListeners() {
+        for(DataChangeListener listener:dataChangeListeners){
+            listener.onDataChanged();
         }
     }
 
