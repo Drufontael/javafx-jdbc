ee12d2745d7c1da7eb50f2f3c8c09306fe70c09c
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-12-02 07:34:51 -0300

    :recicle: refactor:Atualizando departamento

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -4,6 +4,7 @@ import application.Main;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -11,10 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
@@ -36,6 +34,8 @@ public class DepartmentListController implements Initializable, DataChangeListen
     @FXML
     private TableColumn<Department,String> tableColumnName;
     @FXML
+    private TableColumn<Department,Department> tableColumnEDIT;
+    @FXML
     private Button btNew;
     private ObservableList<Department> obsList;
 
@@ -43,7 +43,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
     public void onBtNewAction(ActionEvent event)
     {
         Department obj=new Department();
-        createDialogform(obj,"DepartmentForm.fxml", Utils.currentStage(event));
+        createDialogForm(obj,"DepartmentForm.fxml", Utils.currentStage(event));
 
     }
 
@@ -72,9 +72,10 @@ public class DepartmentListController implements Initializable, DataChangeListen
         List<Department> list=service.findAll();
         obsList = FXCollections.observableArrayList(list);
         tableViewDepartment.setItems(obsList);
+        initEditButtons();
     }
 
-    public void createDialogform(Department obj,String absoluteName,Stage stageParent){
+    public void createDialogForm(Department obj, String absoluteName, Stage stageParent){
         try{
             FXMLLoader loader=new FXMLLoader(getClass().getResource(absoluteName));
             Pane pane=loader.load();
@@ -102,4 +103,24 @@ public class DepartmentListController implements Initializable, DataChangeListen
     public void onDataChanged() {
         updateTableView();
     }
+
+    private void initEditButtons() {
+        tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnEDIT.setCellFactory(param -> new TableCell<Department, Department>() {
+            private final Button button = new Button("edit");
+            @Override
+            protected void updateItem(Department obj, boolean empty) {
+                super.updateItem(obj, empty);
+                if (obj == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(button);
+                button.setOnAction(
+                        event -> createDialogForm(
+                                obj, "/gui/DepartmentForm.fxml",Utils.currentStage(event)));
+            }
+        });
+    }
+
 }
